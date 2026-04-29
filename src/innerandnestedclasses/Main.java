9c108d84916b27a0968384a70ac814ff06417533
Jorge Carlos Garcia Medina <jorgecarlos.garcia@globant.com>
2026-04-28 19:40:40 -0500

    Agregando un ejemplito de Anonymous class

--- src/innerandnestedclasses/Main.java
+++ src/innerandnestedclasses/Main.java
@@ -3,6 +3,7 @@ package innerandnestedclasses;
 import model.PersonaJuridica;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -14,6 +15,28 @@ public class Main {
                 new PersonaJuridica("RUC", "20130987671"));
 
         pjList.sort(new PersonaJuridica.ComparadorPersonas<>());
+        pjList.forEach(System.out::println);
+
+        pjList.forEach(personaJuridica -> personaJuridica.setRazonSocial("Empresa "));
+
+        var anonymousComparator = new Comparator<PersonaJuridica>(){
+            @Override
+            public int compare(PersonaJuridica o1, PersonaJuridica o2) {
+                return o1.getDocumentNumber().compareTo(o2.getDocumentNumber());
+            }
+        };
+
+        pjList.sort(anonymousComparator);
+        pjList.forEach(System.out::println);
+
+        pjList.sort(new Comparator<PersonaJuridica>() {
+            @Override
+            public int compare(PersonaJuridica o1, PersonaJuridica o2) {
+                return o1.getRazonSocial().compareTo(o2.getRazonSocial());
+            }
+        });
+
+        pjList.sort(anonymousComparator);
         pjList.forEach(System.out::println);
     }
 }
